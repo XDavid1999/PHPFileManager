464062be382acd784c80fedff8ff0a51c4c6d731
XDavid1999 <xdavidheredia99@gmail.com>
2022-08-04 12:49:52 +0200

    Adaptación a vista móvil close #9

--- resources/views/dashboard/ownContent.blade.php
+++ resources/views/dashboard/ownContent.blade.php
@@ -1,44 +1,72 @@
 <div>
-    @include('dashboard.uploadContentForm', ['currentDirectory' => $currentDirectory], ['categories' => $categories])
-    @include('dashboard.moveFolderForm', ['categories' => $categories])
-    @include('dashboard.createFolderForm')
-    @include('dashboard.shareContentForm', ['users' => $users, 'files' => $files])
+    @include('dashboard.forms.uploadContentForm', ['currentDirectory' => $currentDirectory], ['categories' => $categories])
+    @include('dashboard.forms.moveFolderForm', ['categories' => $categories])
+    @include('dashboard.forms.createFolderForm')
+    @include('dashboard.forms.shareContentForm', ['users' => $users, 'files' => $files])
 
-    <div class="d-flex justify-content-between p-4 mx-2">
+    <style>
+        .ownContentHead{
+            display: flex;
+            justify-content: space-between;
+            padding: 20px;
+            margin: 0px 20px;
+        }
+
+        .icons{
+            margin: 0px 20px;
+        }
+
+        @media (max-width: 768px) {
+            .ownContentHead{
+                flex-direction: column;
+                align-items: center;
+                justify-content: center;
+                padding: 0px;
+                margin: 0px;
+            }
+
+            .icons{
+                width: 20px !important;
+            }
+        }
+
+    </style>
+
+    <div class="ownContentHead">
         <h2 class=" text-center">My Content - {{$currentDirectory}}</h2>
         <div class="d-flex">
             @if (str_contains($currentDirectory, '/'))
                 <a class="ms-2" href="{{ route('dashboard', 'path='.getParentDirectory($currentDirectory)) }}">
                     <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor"
-                        class="bi bi-arrow-left-circle mx-4" viewBox="0 0 16 16">
+                        class="bi bi-arrow-left-circle icons" viewBox="0 0 16 16">
                         <path fill-rule="evenodd"
                             d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z" />
                     </svg>
                 </a>
             @endif
             <svg onclick="displayCreateFolderDialog()" xmlns="http://www.w3.org/2000/svg" width="40" height="40"
-                fill="#DB7DFA" class="bi bi-folder-plus mx-4" viewBox="0 0 16 16">
+                fill="#DB7DFA" class="bi bi-folder-plus icons" viewBox="0 0 16 16">
                 <path
                     d="m.5 3 .04.87a1.99 1.99 0 0 0-.342 1.311l.637 7A2 2 0 0 0 2.826 14H9v-1H2.826a1 1 0 0 1-.995-.91l-.637-7A1 1 0 0 1 2.19 4h11.62a1 1 0 0 1 .996 1.09L14.54 8h1.005l.256-2.819A2 2 0 0 0 13.81 3H9.828a2 2 0 0 1-1.414-.586l-.828-.828A2 2 0 0 0 6.172 1H2.5a2 2 0 0 0-2 2zm5.672-1a1 1 0 0 1 .707.293L7.586 3H2.19c-.24 0-.47.042-.683.12L1.5 2.98a1 1 0 0 1 1-.98h3.672z" />
                 <path
                     d="M13.5 10a.5.5 0 0 1 .5.5V12h1.5a.5.5 0 1 1 0 1H14v1.5a.5.5 0 1 1-1 0V13h-1.5a.5.5 0 0 1 0-1H13v-1.5a.5.5 0 0 1 .5-.5z" />
             </svg>
             <svg onclick="displayMoveDialog()" xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="#F774A3"
-                class="bi bi-folder-symlink mx-4" viewBox="0 0 16 16">
+                class="bi bi-folder-symlink icons" viewBox="0 0 16 16">
                 <path
                     d="m11.798 8.271-3.182 1.97c-.27.166-.616-.036-.616-.372V9.1s-2.571-.3-4 2.4c.571-4.8 3.143-4.8 4-4.8v-.769c0-.336.346-.538.616-.371l3.182 1.969c.27.166.27.576 0 .742z" />
                 <path
                     d="m.5 3 .04.87a1.99 1.99 0 0 0-.342 1.311l.637 7A2 2 0 0 0 2.826 14h10.348a2 2 0 0 0 1.991-1.819l.637-7A2 2 0 0 0 13.81 3H9.828a2 2 0 0 1-1.414-.586l-.828-.828A2 2 0 0 0 6.172 1H2.5a2 2 0 0 0-2 2zm.694 2.09A1 1 0 0 1 2.19 4h11.62a1 1 0 0 1 .996 1.09l-.636 7a1 1 0 0 1-.996.91H2.826a1 1 0 0 1-.995-.91l-.637-7zM6.172 2a1 1 0 0 1 .707.293L7.586 3H2.19c-.24 0-.47.042-.683.12L1.5 2.98a1 1 0 0 1 1-.98h3.672z" />
             </svg>
             <svg onclick="displayUploadDialog()" xmlns="http://www.w3.org/2000/svg" width="40" height="40"
-                fill="#E08B74" class="bi bi-cloud-arrow-up mx-4" viewBox="0 0 16 16">
+                fill="#E08B74" class="bi bi-cloud-arrow-up icons" viewBox="0 0 16 16">
                 <path fill-rule="evenodd"
                     d="M7.646 5.146a.5.5 0 0 1 .708 0l2 2a.5.5 0 0 1-.708.708L8.5 6.707V10.5a.5.5 0 0 1-1 0V6.707L6.354 7.854a.5.5 0 1 1-.708-.708l2-2z" />
                 <path
                     d="M4.406 3.342A5.53 5.53 0 0 1 8 2c2.69 0 4.923 2 5.166 4.579C14.758 6.804 16 8.137 16 9.773 16 11.569 14.502 13 12.687 13H3.781C1.708 13 0 11.366 0 9.318c0-1.763 1.266-3.223 2.942-3.593.143-.863.698-1.723 1.464-2.383zm.653.757c-.757.653-1.153 1.44-1.153 2.056v.448l-.445.049C2.064 6.805 1 7.952 1 9.318 1 10.785 2.23 12 3.781 12h8.906C13.98 12 15 10.988 15 9.773c0-1.216-1.02-2.228-2.313-2.228h-.5v-.5C12.188 4.825 10.328 3 8 3a4.53 4.53 0 0 0-2.941 1.1z" />
             </svg>
             <svg onclick="displayshareContentDialog()" xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="#F7BB74"
-                class="bi bi-share-fill px-2" viewBox="0 0 16 16">
+                class="bi bi-share-fill icons" viewBox="0 0 16 16">
                 <path
                     d="M11 2.5a2.5 2.5 0 1 1 .603 1.628l-6.718 3.12a2.499 2.499 0 0 1 0 1.504l6.718 3.12a2.5 2.5 0 1 1-.488.876l-6.718-3.12a2.5 2.5 0 1 1 0-3.256l6.718-3.12A2.5 2.5 0 0 1 11 2.5z" />
             </svg>
@@ -46,7 +74,7 @@
     </div>
     <div class="d-flex justify-content-center px-5 pb-5 row">
         @foreach ($directories as $directory=>$d)
-        <div class="p-4 col col-3 mt-2">
+        <div class="p-4 col col-lg-3 col-md-4 col-md-12 mt-2">
             <div class="d-flex justify-content-center my-2">
                 <a href="{{ route('dashboard', 'path='.$d['path']) }}" xmlns="http://www.w3.org/2000/svg">
                     <svg width="100" height="100" fill="#FF8A6D" class="bi bi-folder2" viewBox="0 0 16 16">
@@ -84,7 +112,7 @@
         </div>
         @endforeach
         @foreach ($files as $file=>$f)
-        <div class="p-4 col col-3">
+        <div class="p-4 col col-lg-3 col-md-4 col-md-12">
             <div class="d-flex justify-content-center">
                 <form id="{{$f['name']}}" action="{{ route('privacity') }}" method="POST">
                     @csrf
